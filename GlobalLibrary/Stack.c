6b4bdc13d9e6e9f5536da4959c681eeb01e560fe
prem-jeet <62586700+prem-jeet@users.noreply.github.com>
2024-02-16 11:26:45 +0000

    finished Stack.c

--- GlobalLibrary/Stack.c
+++ GlobalLibrary/Stack.c
@@ -1,6 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "Stack.h"
 
-int 
+Stack *createStack(int size)
+{
+    Stack *s = malloc(1 * sizeof(Stack));
+    s->top = -1;
+    s->size = size;
+    s->stack = calloc(size, sizeof(int));
+    return s;
+}
+
+bool isStackFull(Stack *s)
+{
+    return s->top == s->size - 1;
+}
+bool isStackEmpty(Stack *s)
+{
+    return s->top == -1;
+}
+
+int push(Stack *s, int item){
+    if(isStackFull(s)) return -1;
+    s->stack[++(s->top)] = item;
+    return item; 
+}
+
+int pop(Stack *s){
+    if(isStackEmpty(s)) return -1;
+    return s->stack[s->top--];
+
+}
+
+void printStack(Stack *s){
+    printf("\n");
+    for(int i=0;i <s->size;i++){
+        if(i<=s->top) printf("%d |", s->stack[i]);
+        else printf(" |");
+    }
+    printf("\n");
+}
--- GlobalLibrary/Stack.h
+++ GlobalLibrary/Stack.h
@@ -1,13 +1,15 @@
-#include<stdbool.h>
+#include <stdbool.h>
 
-typedef struct{
-    int (*stack)[];
-    int size;int top;
-}Stack;
+typedef struct
+{
+    int* stack;
+    int size;
+    int top;
+} Stack;
 
 int push(Stack *, int);
 int pop(Stack *);
 bool isStackFull(Stack *);
 bool isStackEmpty(Stack *);
 void printStack(Stack *);
-Stack* createStack(int);
+Stack *createStack(int);
