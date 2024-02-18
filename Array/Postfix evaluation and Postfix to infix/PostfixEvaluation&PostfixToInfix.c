7c08093315f2d12a62a2df5917c330f577c59f32
prem-jeet prasad <jeet4300795@gmail.com>
2024-02-18 10:59:47 +0000

    code format

--- Array/Postfix evaluation and Postfix to infix/PostfixEvaluation&PostfixToInfix.c
+++ Array/Postfix evaluation and Postfix to infix/PostfixEvaluation&PostfixToInfix.c
@@ -1,71 +1,64 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 #include "../../GlobalLibrary/Stack.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool isOperator(char);
 int performOperation(int, int, char);
 int evaluatePostfix(char *);
-int main()
-{
-    // Expression input code - begin
-    printf("Enter Postfix Expression : ");
-    char *postfixExpression = NULL;
-    size_t size = 0;
-    getline(&postfixExpression, &size, stdin);
-    int expressionLen = strlen(postfixExpression);
-    postfixExpression[expressionLen - 1] = '\0';
-    // Expression input code - end
+int main() {
+  // Expression input code - begin
+  printf("Enter Postfix Expression : ");
+  char *postfixExpression = NULL;
+  size_t size = 0;
+  getline(&postfixExpression, &size, stdin);
+  int expressionLen = strlen(postfixExpression);
+  postfixExpression[expressionLen - 1] = '\0';
+  // Expression input code - end
 
-    printf("\nExpression output : %d\n", evaluatePostfix(postfixExpression));
-    return 1;
+  printf("\nExpression output : %d\n", evaluatePostfix(postfixExpression));
+  return 1;
 }
 
-int evaluatePostfix(char *postfixExpression)
-{
-    char *t = postfixExpression;
-    printf("\ninput : %s", postfixExpression);
-    Stack *operandStack = createStack(strlen(postfixExpression));
-    while (*t)
-    {
-        printf("\nOperand stack");
-        printStack(operandStack);
-        if (!isOperator(*t))
-        {
-            push(operandStack, *t - '0');
-            t++;
-            continue;
-        }
-        printf("Operator : %c\n", *t);
-        int secondOperand = pop(operandStack);
-        int firstOperand = pop(operandStack);
-        push(operandStack, performOperation(firstOperand, secondOperand, *t));
-        t++;
-    }
+int evaluatePostfix(char *postfixExpression) {
+  char *t = postfixExpression;
+  printf("\ninput : %s", postfixExpression);
+  Stack *operandStack = createStack(strlen(postfixExpression));
+  while (*t) {
     printf("\nOperand stack");
     printStack(operandStack);
-    return pop(operandStack);
+    if (!isOperator(*t)) {
+      push(operandStack, *t - '0');
+      t++;
+      continue;
+    }
+    printf("Operator : %c\n", *t);
+    int secondOperand = pop(operandStack);
+    int firstOperand = pop(operandStack);
+    push(operandStack, performOperation(firstOperand, secondOperand, *t));
+    t++;
+  }
+  printf("\nOperand stack");
+  printStack(operandStack);
+  return pop(operandStack);
 }
 
-bool isOperator(char c)
-{
-    return c == '+' || c == '-' || c == '/' || c == '*' || c == '^';
+bool isOperator(char c) {
+  return c == '+' || c == '-' || c == '/' || c == '*' || c == '^';
 }
-int performOperation(int firstOperand, int secondOperand, char operator)
-{
-    switch (operator)
-    {
-    case '+':
-        return firstOperand + secondOperand;
-    case '-':
-        return firstOperand - secondOperand;
-    case '*':
-        return firstOperand * secondOperand;
-    case '/':
-        return firstOperand / secondOperand;
-    case '^':
-        return pow(firstOperand, secondOperand);
-    }
+int performOperation(int firstOperand, int secondOperand, char operator) {
+  switch (operator) {
+  case '+':
+    return firstOperand + secondOperand;
+  case '-':
+    return firstOperand - secondOperand;
+  case '*':
+    return firstOperand * secondOperand;
+  case '/':
+    return firstOperand / secondOperand;
+  case '^':
+    return pow(firstOperand, secondOperand);
+  }
 }
