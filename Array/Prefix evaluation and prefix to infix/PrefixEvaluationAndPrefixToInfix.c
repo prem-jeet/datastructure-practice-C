e85e3f19d9198186b421b41adf404c18aa02aaa2
prem-jeet <62586700+prem-jeet@users.noreply.github.com>
2024-02-20 16:48:06 +0000

    added code to evaluate prefix expression

--- Array/Prefix evaluation and prefix to infix/PrefixEvaluationAndPrefixToInfix.c
+++ Array/Prefix evaluation and prefix to infix/PrefixEvaluationAndPrefixToInfix.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 #include "../../GlobalLibrary/GenericStack.h"
 
+bool isOperator(char);
+float performOperation(float, float, char);
+float evaluatePrefix(char *);
+void printFloatStack(Stack *);
 int main()
 {
     char *prefixExpression = NULL;
@@ -13,7 +18,73 @@ int main()
     int len = strlen(prefixExpression);
     prefixExpression[len - 1] = '\0';
 
-    
-
+    Stack *operandStack = createStack(len);
+    float result = evaluatePrefix(prefixExpression);
+    printf("\nThe Prefix Expression evaluates to : %0.2f", result);
     return 1;
+}
+
+float evaluatePrefix(char *prefixExpression)
+{
+    int index = strlen(prefixExpression);
+    Stack *operandStack = createStack(index);
+    while (--index > -1)
+    {
+        char curr = prefixExpression[index];
+        float *f = malloc(sizeof(float));
+        printf("\nOperand Stack:");
+        printFloatStack(operandStack);
+        if (isOperator(prefixExpression[index]))
+        {
+            printf("Operator : %c", curr);
+            float *firstOperand = pop(operandStack);
+            float *secondOperand = pop(operandStack);
+
+            *f = performOperation(*firstOperand, *secondOperand, curr);
+        }
+        else
+        {
+            *f = curr - '0';
+        }
+
+        push(operandStack, f);
+    }
+    return *(float *)pop(operandStack);
+}
+
+bool isOperator(char c)
+{
+    return c == '+' || c == '-' || c == '/' || c == '*' || c == '^';
+}
+
+float performOperation(float firstOperand, float secondOperand, char operator)
+{
+    switch (operator)
+    {
+    case '+':
+        return firstOperand + secondOperand;
+    case '-':
+        return firstOperand - secondOperand;
+    case '*':
+        return firstOperand * secondOperand;
+    case '/':
+        return firstOperand / secondOperand;
+    case '^':
+        return pow(firstOperand, secondOperand);
+    }
+}
+
+void printFloatStack(Stack *s)
+{
+    printf("\n");
+    for (int i = 0; i < s->size; i++)
+    {
+        if (i <= s->top)
+        {
+            printf("%0.2f  | ", *(float *)s->stack[i]);
+            continue;
+        }
+        printf("  | ");
+    }
+    printf("\n");
 }
