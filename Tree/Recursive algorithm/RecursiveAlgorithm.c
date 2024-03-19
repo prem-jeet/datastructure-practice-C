706c0634c93bc1f96c584e92316265f1c0d2c368
prem-jeet prasad <jeet4300795@gmail.com>
2024-03-19 10:37:47 +0000

    iterative algo to count number of leafs

--- Tree/Recursive algorithm/RecursiveAlgorithm.c
+++ Tree/Recursive algorithm/RecursiveAlgorithm.c
@@ -8,7 +8,7 @@ gcc RecursiveAlgorithm.c ../TreeHelper.c ../../GlobalLibrary/GenericQueue.c
 #include "../TreeHelper.h"
 
 int countLeafsRecursive(TreeNode);
-
+int countLeafsIterative(TreeNode);
 /* Tree structure
     1
  /    \
@@ -21,7 +21,9 @@ int countLeafsRecursive(TreeNode);
 int main() {
   int tree[] = {1, 2, 3, 0, 4, 5, 6, 0, 0, 0, 0, 0, 7, 0, 0};
   TreeNode root = createTree(tree, 15);
-  printf("Number of leaf nodes : %d", countLeafsRecursive(root));
+  printf("Number of leaf nodes : %d\n", countLeafsRecursive(root));
+  printf("Number of leaf nodes : %d\n", countLeafsIterative(root));
+
   return 1;
 }
 
@@ -31,4 +33,23 @@ int countLeafsRecursive(TreeNode root) {
   if (!root->left && !root->right)
     return 1;
   return countLeafsRecursive(root->left) + countLeafsRecursive(root->right);
+}
+int countLeafsIterative(TreeNode root) {
+  if (!root)
+    return 0;
+  int count = 0;
+  Queue q = createQueue();
+  enqueue(q, root);
+  while (!isEmpty(q)) {
+    TreeNode curr = dequeue(q);
+    if (!(curr->left && curr->right))
+      count++;
+    else {
+      if (curr->left)
+        enqueue(q, curr->left);
+      if (curr->right)
+        enqueue(q, curr->right);
+    }
+  }
+  return count;
 }
